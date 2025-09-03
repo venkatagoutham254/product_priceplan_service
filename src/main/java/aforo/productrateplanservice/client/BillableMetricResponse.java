08f32d3895efe94f127ff988ca3e019dd01a0095
Vaishnav Jayaraj <vaishu4112002@gmail.com>
2025-09-03 08:32:03 +0100

    status logic and patch execution

--- src/main/java/aforo/productrateplanservice/client/BillableMetricResponse.java
+++ src/main/java/aforo/productrateplanservice/client/BillableMetricResponse.java
@@ -1,13 +1,13 @@
 package aforo.productrateplanservice.client;
 
-
-
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 @Data
 public class BillableMetricResponse {
-    private Long metricId;
+    @JsonProperty("billableMetricId")
+    private Long metricId; // mapped from billableMetricId
     private String metricName;
     private Long productId;
     private String version;
@@ -15,6 +15,7 @@ public class BillableMetricResponse {
     private String description;
     private String aggregationFunction;
     private String aggregationWindow;
+    private String status; // DRAFT/ACTIVE, used for filtering
     private List<UsageConditionDTO> usageConditions;
 }
 
